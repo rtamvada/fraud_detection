e3310a3f6364cb188dbb22b0712b02db68f36573
RT Tamvada <raviteja.tamvada@gmail.com>
2022-03-25 01:53:06 +0530

    Added 3 new ML models

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,12 @@
 
+
+def model_1():
+    print("Adding code for ML model 1")
+
+def model_2():
+    print("Adding code for ML model 2")
+
+def model_3():
+    print("Adding code for ML model 3")
+
 print("Adding all ML code here.")
